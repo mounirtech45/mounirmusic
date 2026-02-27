b62e3909f2a42954f8dca205d15960fce385d687
mounirtech45 <mounirkherroubi2@gmail.com>
2026-02-27 04:40:49 +0100

    Add files via upload

--- core/admins.py
+++ core/admins.py
@@ -16,12 +16,28 @@ You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>
 """
 
-from core.song import Song
-from core.admins import is_sudo, is_admin
-from core.stream import app, ydl, safone, pytgcalls, skip_stream, start_stream
-from core.groups import (
-    get_group, get_queue, set_group, set_title, all_groups, clear_queue,
-    set_default, shuffle_queue)
-from core.funcs import (
-    search, check_yt_url, extract_args, generate_cover, delete_messages,
-    get_spotify_playlist, get_youtube_playlist)
+from config import config
+
+
+async def is_sudo(message):
+    if message.from_user and message.from_user.id in config.SUDOERS:
+        return True
+    else:
+        return False
+
+
+async def is_admin(message):
+    if message.from_user and (
+        message.from_user.id
+        in [
+            admin.user.id
+            for admin in (await message.chat.get_members(filter="administrators"))
+        ]
+    ):
+        return True
+    elif message.from_user and message.from_user.id in config.SUDOERS:
+        return True
+    elif message.sender_chat and message.sender_chat.id == message.chat.id:
+        return True
+    else:
+        return False
